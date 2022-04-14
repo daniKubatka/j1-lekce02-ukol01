8fa86e47289bfab1912e07f8d7cfd6ed9bae2ea7
daniKubatka <Bluappl!>
2022-04-14 08:33:33 +0200

    Zelva nakresli domecek

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -3,11 +3,35 @@ package net.sevecek.turtle;
 import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
+    Turtle zofka;
 
     public void main(String[] args) {
-        Turtle zofka;
-
         zofka = new Turtle();
+
+        nakresliDomecek(zofka);
+
+    }
+
+
+    public void nakresliCtverec(Turtle zelva){
+        for (int i = 0; i < 4; i++) {
+            zelva.turnRight(90);
+            zelva.move(100);
+        }
+    }
+
+    public void nakresliTrojuhelnik(Turtle zelva){
+        zelva.turnRight(45);
+        zelva.move(72);
+        zelva.turnRight(90);
+        zelva.move(72);
+        zelva.turnRight(135);
+        zelva.move(100);
+    }
+
+    private void nakresliDomecek(Turtle zelva){
+        nakresliCtverec(zelva);
+        nakresliTrojuhelnik(zelva);
     }
 
 }
