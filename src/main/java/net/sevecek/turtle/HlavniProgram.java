c52f8aac0f1ea62bada9ec6503fca36fbd207d71
daniKubatka <bluappl!>
2022-04-14 10:29:28 +0200

    Zelva nakresli prasatko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,15 +8,23 @@ public class HlavniProgram {
     public void main(String[] args) {
         zofka = new Turtle();
 
-        nakresliDomecek(zofka);
+        nakresliPrasatko(zofka);
 
     }
-
 
     public void nakresliCtverec(Turtle zelva){
         for (int i = 0; i < 4; i++) {
             zelva.turnRight(90);
             zelva.move(100);
+        }
+    }
+
+    public void nakresliObdelnik(Turtle zelva){
+        for (int i = 0; i < 2; i++) {
+            zelva.move(100);
+            zelva.turnRight(90);
+            zelva.move(150);
+            zelva.turnRight(90);
         }
     }
 
@@ -32,6 +40,36 @@ public class HlavniProgram {
     private void nakresliDomecek(Turtle zelva){
         nakresliCtverec(zelva);
         nakresliTrojuhelnik(zelva);
+    }
+
+    private void nakresliPrasatko(Turtle zelva){
+        zelva.turnLeft(90);
+        nakresliTrojuhelnik(zelva);
+        zelva.turnLeft(180);
+        nakresliTelo(zelva);
+    }
+
+    private void nakresliTelo(Turtle zelva){
+        nakresliObdelnik(zelva);
+        zelva.turnRight(180);
+        nohy(zelva);
+        zelva.penUp();
+        zelva.turnRight(135);
+        zelva.move(150);
+        zelva.penDown();
+        zelva.turnRight(90);
+        nohy(zelva);
+    }
+
+    private void nohy (Turtle zelva){
+        zelva.turnRight(45);
+        zelva.move(30);
+        zelva.turnRight(180);
+        zelva.move(30);
+        zelva.turnRight(90);
+        zelva.move(30);
+        zelva.turnRight(180);
+        zelva.move(30);
     }
 
 }
